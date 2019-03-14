fdb20b7f5d645274b664999c8014c40033723956
Tanel Ismael <tanel.ismael@khk.ee>
2019-03-14 09:35:29 +0200

    addressbook and stuff

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,11 +1,11 @@
 describe('Address Book', function() {
     it('should be able to add a contact', function () {
         var addressBook = new AddressBook (),
-            thisContact = new thisContact();
+            thisContact = new Contact();
 
-        AddressBook.addContact(thisContact);
+        addressBook.addContact(thisContact);
 
-        expect(AddressBook.getContact(0)).toBe(thisContact);
+        expect(addressBook.getContact(0)).toBe(thisContact);
 
     });
 });
